d33fc32fb59cff001c9f6462727a889443cdb363
samLi <s00001sam@gmail.com>
2025-02-15 02:59:51 +0800

    Add composeCompiler to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,10 @@ android {
         compose = true
         buildConfig = true
     }
+    composeCompiler {
+        reportsDestination = layout.buildDirectory.dir("compose_compiler")
+        metricsDestination = layout.buildDirectory.dir("compose_compiler")
+    }
 }
 
 dependencies {
